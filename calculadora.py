0f86fb619fe033640b4cce3bda598e140abf49e7
Wiwer <rafael.af.motta@gmail.com>
2026-07-16 17:06:50 -0300

    Alterando para 17

--- calculadora.py
+++ calculadora.py
@@ -35,7 +35,7 @@ def api_dividir():
 @app.route('/')
 def home():
     return jsonify({
-        'mensagem': '🚀 DEPLOY AUTOMATICO FUNCIONANDO!',
+        'mensagem': '🚀 DEPLOY AUTOMATICO FUNCIONANDO! 17:06',
         'endpoints': {
             '/somar?a=2&b=3': 'Soma dois números',
             '/dividir?a=10&b=2': 'Divide dois números'
